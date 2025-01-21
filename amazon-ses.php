1352e0919d5d5a561798bdc424ca9af1965c2047
amikecallies <acallies15@apu.edu>
2025-01-20 18:47:33 -0800

    Update amazon-ses.php

--- amazon-ses.php
+++ amazon-ses.php
@@ -32,10 +32,10 @@ try {
     $mail->addAddress('callieskeith@gmail.com', 'Keith Callies');
 
     // Replace smtp_username with your Amazon SES SMTP user name.
-    $mail->Username = 'AKIA4HJMF54KR6RMZO7G';
-
+    $mail->Username = getenv('AMAZON_USER');
+    
     // Replace smtp_password with your Amazon SES SMTP password.
-    $mail->Password = 'BIaPiubigPTFVZS6+vl90lygXYC4pn8XrkT++DfgaSkI';
+    $mail->Password = getenv('AMAZON_PASSWORD');
         
     // Specify a configuration set. If you do not want to use a configuration
     // set, comment or remove the next line.
